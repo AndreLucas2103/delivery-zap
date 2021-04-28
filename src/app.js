11bc021810cc468456448f7f2362b3770f822e47
MateusPereiraS <75630013+MateusPereiraS@users.noreply.github.com>
2021-04-27 23:21:22 -0300

    1.0

--- src/app.js
+++ src/app.js
@@ -7,11 +7,7 @@ const app = express()
 
 
 //Middleware
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg")
-    res.locals.error_msg = req.flash("error_msg")
-    res.locals.error = req.flash("error")
-    next();
+
 
 // ----- Body Parser -----------------------------------------------------------------------------------------------------------------------
 
