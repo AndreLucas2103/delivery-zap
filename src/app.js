bf73a10df242715a2751edfd537d365e29bb235a
Andre <p4n1c0021@gmail.com>
2021-04-29 19:58:57 -0300

    1.0

--- src/app.js
+++ src/app.js
@@ -127,9 +127,11 @@ app.get('/registro', (req, res) => {
 // Rotas para USUARIOS
 const acessoUsuario = require("./routes/usuario/acessos/acessos")
 const usuarioDashboard = require("./routes/usuario/dashboard/dashboard")
+const usuarioUsuario = require("./routes/usuario/usuario/usuario")
 
 app.use('/acessos', acessoUsuario)
 app.use('/dashboard', usuarioDashboard)
+app.use('/usuario', usuarioUsuario)
 
 
 // ---- Rotas Para TESTES -----------------------------------------------------------------------------------------------------------------------------
