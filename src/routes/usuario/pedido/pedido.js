5f151eb4f08af1dec74c662ac9434af94a679313
Andre <p4n1c0021@gmail.com>
2021-06-15 23:56:52 -0300

    ok

--- src/routes/usuario/pedido/pedido.js
+++ src/routes/usuario/pedido/pedido.js
@@ -29,7 +29,7 @@ router.get('/pedidos', async (req, res) => {
         let userEstabelecimentos = []
         req.user.estabelecimentosSelecionados.forEach(element => { userEstabelecimentos.push(element.idEstabelecimento) }) 
         
-        let {conteudo, dataInicio, dataFim} = req.query
+        let {conteudo, dataInicio, dataFim, limit, paginate} = req.query
         let userTimeZone = req.user.timeZone.substr(1, 2) // capturo o fuso horario do perfil do usuario logado, capturando apenas os dois digitos necesarios e transformando em NUMBER
 
         // verifico se existe algum valor antes de transformar em um object e depois mapear, retonar apenas o valor desejado de cada objeto dentro de um array, caso não tenha eu passo {} na pesquisa
@@ -69,7 +69,7 @@ router.get('/pedidos', async (req, res) => {
         dataFim ? find_dataFim = moment(dataFim).add(userTimeZone, 'H').format() : find_dataFim = moment().format()
 
         // realizo toda a buscados pedidos conforme as condicoes acima
-        let pedidos = await Pedido.find({
+        let query = {
             $and: [
                 find_conteudo, find_pagamentoTipo, find_pagamentoForma, find_entregador, find_tipoEntrega,
                 {$or: find_situacao},
@@ -77,10 +77,20 @@ router.get('/pedidos', async (req, res) => {
                 {'dataCriacao': {$gte: find_dataInicio, $lte: find_dataFim}},
                 {'idEstabelecimento': userEstabelecimentos}
             ]
-        }).lean().populate('idEstabelecimento')
+        }
+
+        let totalPage = await Pedido.countDocuments(query)
+
+        limit ? find_limit = Number(limit) : find_limit = 20
+        paginate ? find_paginate = Number(paginate) : find_paginate = 1
+        let skip = find_limit * (find_paginate-1)
+
+        console.log(limit)
+        console.log(skip)
+
+        let pedidos = await Pedido.find(query).skip(skip).limit(find_limit).lean().populate('idEstabelecimento')
         let entregadores = await Entregador.find({'estabelecimentos.idEstabelecimento': userEstabelecimentos}).populate('estabelecimentos.idEstabelecimento').lean()
 
-        
         res.render('usuarios/pedido/pedidos', {
             pedidos: pedidos,
             entregadores: entregadores,
@@ -93,12 +103,64 @@ router.get('/pedidos', async (req, res) => {
             entregador: req.query.entregador,
             tipoEntrega: req.query.tipoEntrega,
             situacao: req.query.situacao,
-            pagamento: req.query.pagamento
+            pagamento: req.query.pagamento,
+
+            pagination: {
+                // api handlebars paginate
+                page: find_paginate,
+                pageCount: totalPage/find_limit,
+
+                totalPage: totalPage,
+                limitPage: find_limit,
+                startPage: pedidos.length == 0 ? 0 : skip+1,
+                endPage: skip + pedidos.length
+            },
         })
 
     } catch (err) {
         console.log(err)
     }
+})
+
+router.get('/teste', (req, res) => {
+        pedido ={
+            infoEntrega: { nomeCliente: '12', telefone: '12', taxaEntrega: 0 },
+            pagamento: { pago: false, tipo: 'pagarRecebimento', forma: 'cartaoDebito' },
+            infoTransacao: { pedidoPago: false, pedidoCancelado: false },
+            cancelado: false,
+            finalizado: false,
+            statusAtivo: true,
+            identificacaoPedido: '1623798261499-60a1368deaa13a02685d91fa-ab511433-2921-40e1-afe9-464e94f4d1b587566b31-9bb7-4a20-9106-fce96ad08c4d',
+            produtos: [
+              {
+                _id: "60c178baba13033cc872b6d4",
+                idProduto: "60b273a7be12031f2496afe6",
+                nome: 'COMBO INDIVIDUAL SUPREMO 2 SÓ2',
+                valor: 12,
+                valorTotal: 44,
+                idCategoriaProduto: "60a136a0eaa13a02685d91fe",
+                observacao: '123123',
+                quantidade: 2
+              }
+            ],
+            tipoEntrega: 'retirarEstabelecimento',
+            uuid4Client: '1622679808676-35c000a9-1492-4d41-99dc-1d8ad0251a614fa616ad-3b78-46d0-bc3b-07230ff0eb35709b9f77-64f5-4400-9948-8ce84fd14fcd',
+            idEstabelecimento: "60a1368deaa13a02685d91fa",
+            observacao: '',
+            valorTotal: 44.00000000000014,
+            dataCriacao: new Date()
+        }
+        for (let i = 0; i < 1000; i++) {
+            Pedido(pedido).save().then(() => {
+                console.log(i)
+                if(i == 999){
+                    return res.redirect('/pedido/teste')
+                }
+            }).catch(err => {
+                console.log(err)
+                return
+            })
+        } 
 })
 
 
