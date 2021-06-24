da732e4561c496a6728c7e36530d0088f97be9e9
Andre <p4n1c0021@gmail.com>
2021-06-23 23:23:49 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -17,6 +17,13 @@ const cors = require("cors");
 
 
 // ----- Middleware -----------------------------------------------------------------------------------------------------------------------
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+    else //Se a requisição já é HTTPS 
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
