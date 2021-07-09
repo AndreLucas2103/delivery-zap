cfc8954196adbc3617f889806d38b127715ede21
MateusPereiraS <75630013+MateusPereiraS@users.noreply.github.com>
2021-07-09 00:50:05 -0300

    1

--- src/helpers/handlebars-helpers.js
+++ src/helpers/handlebars-helpers.js
@@ -20,5 +20,32 @@ Handlebars.registerHelper('dataFormatTimeZone', function(userTimeZone, data, for
     return moment(new Date(data)).utcOffset(timeZone).format(formtMoment);
 })
 
+Handlebars.registerHelper('FormatValor', function(valor) {
+  return Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(valor)
+})
 
+Handlebars.registerHelper('situacaoPedido', function(pedido) {
 
+    switch (pedido) {
+        case 'canceled':
+            return ('<i class="fas fa-times-circle text-danger"></i> Cancelado')
+            break;
+        case 'waiting':
+            return ('<i class="fas fa-clock"></i> Aguardando')
+            break;
+        case 'production':
+            return ('<i class="fas fa-spinner text-warning"></i> Produção')
+            break;
+        case 'concluded':
+            return ('<i class="fas fa-check text-success"></i> Concluido')
+            break;
+        case 'delivery':
+            return ('<i class="fas fa-truck"></i> Entrega')
+            break;
+        case 'finished':
+            return ('<i class="fas fa-check-circle text-success"></i> Finalizado')
+            break;
+        default:
+            return ('err')
+    }
+  })
