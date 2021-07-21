342999e28bbacf14f11ec2f7cc6f11ff1284f662
MateusPereiraS <75630013+MateusPereiraS@users.noreply.github.com>
2021-07-21 14:18:42 -0300

    1

--- src/helpers/handlebars-helpers.js
+++ src/helpers/handlebars-helpers.js
@@ -31,46 +31,16 @@ Handlebars.registerHelper('situacaoPedido', function(pedido) {
             return ('<i class="fas fa-times-circle text-danger"></i> Cancelado')
             break;
         case 'waiting':
-            return ('<i class="fas fa-clock"></i> Pedido enviado')
+            return ('<i class="fas fa-clock"></i> Aguardando')
             break;
         case 'production':
-            return ('<i class="fas fa-hamburger"></i> Preparando seu pedido')
+            return ('<i class="fas fa-spinner text-warning"></i> Produção')
             break;
         case 'concluded':
             return ('<i class="fas fa-check text-success"></i> Concluido')
             break;
         case 'delivery':
-            return ('<i class="fas fa-truck"></i> Saindo para entrega')
-            break;
-        case 'finished':
-            return ('<i class="fas fa-check-circle text-success"></i> Finalizado')
-            break;
-        default:
-            return ('err')
-    }
-  })
-
-  Handlebars.registerHelper('FormatValor', function(valor) {
-  return Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(valor)
-})
-
-Handlebars.registerHelper('situacaoPedido', function(pedido) {
-
-    switch (pedido) {
-        case 'canceled':
-            return ('<i class="fas fa-times-circle text-danger"></i> Cancelado')
-            break;
-        case 'waiting':
-            return ('<i class="fas fa-clock"></i> Pedido enviado')
-            break;
-        case 'production':
-            return ('<i class="fas fa-hamburger"></i> Preparando seu pedido')
-            break;
-        case 'concluded':
-            return ('<i class="fas fa-check text-success"></i> Concluido')
-            break;
-        case 'delivery':
-            return ('<i class="fas fa-truck"></i> Saindo para entrega')
+            return ('<i class="fas fa-truck"></i> Entrega')
             break;
         case 'finished':
             return ('<i class="fas fa-check-circle text-success"></i> Finalizado')
